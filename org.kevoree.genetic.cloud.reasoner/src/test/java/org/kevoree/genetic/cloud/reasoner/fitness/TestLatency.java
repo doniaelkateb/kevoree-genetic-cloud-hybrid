5877c37bd4a3cf2b97169b07fc4964d8b1820140
doniaelkateb <donia.elkateb@uni.lu>
2013-09-08 12:01:40 +0200

    latency fitness

---
+++ org.kevoree.genetic.cloud.reasoner/src/test/java/org/kevoree/genetic/cloud/reasoner/fitness/TestLatency.java
@@ -0,0 +1,45 @@
+package org.kevoree.genetic.cloud.reasoner.fitness;
+import org.junit.Test;
+import org.kevoree.ContainerRoot;
+import org.kevoree.KevoreeFactory;
+import org.kevoree.genetic.cloud.library.onlineStore.ItemDB;
+import org.kevoree.genetic.cloud.library.onlineStore.LoadBalancer;
+import org.kevoree.genetic.cloud.library.onlineStore.PaymentDB;
+import org.kevoree.genetic.cloud.library.onlineStore.UserDB;
+import org.kevoree.genetic.cloud.reasoner.SLAModel;
+import org.kevoree.genetic.cloud.reasoner.population.ExamplePopulationFactory;
+import org.kevoree.impl.DefaultKevoreeFactory;
+import org.kevoree.serializer.JSONModelSerializer;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: donia.elkateb
+ * Date: 7/23/13
+ * Time: 5:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestLatency {
+    @Test
+    public static void main(String[] args) {
+
+
+
+
+        ExamplePopulationFactory factory = new ExamplePopulationFactory();
+        KevoreeFactory kfactory = new DefaultKevoreeFactory();
+        List<ContainerRoot> models = factory.createPopulation();
+        ContainerRoot model = models.get(0);
+
+        //print model to check consistency
+        //JSONModelSerializer jsonModel = new JSONModelSerializer();
+        //jsonModel.serialize(model,System.out);
+
+        LatencyFitness fitness = new LatencyFitness();
+        double Latencyfitness = fitness.evaluate(model, "US" );
+        System.out.println(Latencyfitness);
+
+
+    }
+}
